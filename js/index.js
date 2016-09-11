98cdede7bd7376dc0c2efadc60b2f25146731d5d
Umair Suleman <umairsuleman@Umairs-MacBook-Pro.local>
2016-09-11 04:05:51 -0500

    combining all js

--- js/index.js
+++ js/index.js
@@ -16,7 +16,38 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var check = 0;
+var physician_data_text = '{ "physician" : [' +
+'{ "firstName":"John" , "lastName":"Doe" , "speciality":"general" , "clinic":"sugarland" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"dentist" , "clinic":"sugarland 2" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"gyna" , "clinic":"sugarland 2" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"cardio" , "clinic":"sugarland 2" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"allergist" , "clinic":"sugarland 2" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"dermatologist" , "clinic":"sugarland 2" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"microbiologist" , "clinic":"sugarland 2" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"neorologist" , "clinic":"sugarland 2" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"oncologist" , "clinic":"sugarland 2" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" , "speciality":"pediatrician" , "clinic":"richmond" } ]}', 
+speciality_data_text = '{ "speciality" : [' +
+'{ "name":"general" },' +
+'{ "name":"dentist" },' +
+'{ "name":"gyna" },' +
+'{ "name":"cardio" },' +
+'{ "name":"allergist" },' +
+'{ "name":"dermatologist" },' +
+'{ "name":"microbiologist" },' +
+'{ "name":"neorologist" },' +
+'{ "name":"oncologist" },' +
+'{ "name":"pediatrician" } ]}', 
+clinic_data_text = '{ "clinic" : [' +
+'{ "clinicName":"sugarland" , "streetAddress":"4710 Abingdon Ct" , "city":"sugar land" , "state":"texas" , "zip":"77449" },' +
+'{ "clinicName":"sugarland 2" , "streetAddress":"5322 Mornington Dr" , "city":"sugar land" , "state":"texas" , "zip":"77498" },' +
+'{ "clinicName":"richmond" , "streetAddress":"5114 Stone Island Ct" , "city":"richmond" , "state":"texas" , "zip":"77407" } ]}', 
+physician_obj = JSON.parse(physician_data_text), 
+speciality_obj = JSON.parse(speciality_data_text), 
+clinic_obj = JSON.parse(clinic_data_text), 
+onlineCheck = 0, 
+timeNew = new Date();
+timeNew.setSeconds(timeNew.getSeconds() + 3);
 var app = {
     // Application Constructor
     initialize: function() {
@@ -36,71 +67,45 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('loading');
     },
-    onDeviceOnline: function() {
-        app.deviceOnline();
-    },
-    onDeviceOffline: function() {
-        app.deviceOffline();
-    },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        document.addEventListener('online', this.onDeviceOnline, false);
-        document.addEventListener('offline', this.onDeviceOffline, false);
-        // if((navigator.network.connection.type).toUpperCase() == "NONE" && (navigator.network.connection.type).toUpperCase() == "UNKNOWN") {
-        //     this.deviceOffline();
-        // }
-        // else {
-        //     this.deviceOnline();
-        // }
-        var splashElement = document.getElementById(id);
-        var beginApp = document.getElementById('deviceready');
-        //var listeningElement = parentElement.querySelector('.listening');
-        //var receivedElement = parentElement.querySelector('.received');
-        console.log('Check: ' + check);
-        splashElement.setAttribute('style', 'display:none;');
-        beginApp.setAttribute('style', 'display:block;');
+        if((navigator.network.connection.type).toUpperCase() == "NONE" && (navigator.network.connection.type).toUpperCase() == "UNKNOWN") {
+            this.deviceOffline();
+        }
+        else {
+            this.deviceOnline();
+        }
+        var splashElement = document.getElementById(id), 
+        beginApp = document.getElementById('deviceready'),
+        timeNow = new Date(),
+        timeDiff = timeNew - timeNow;
+        console.log(timeDiff);
+        this.contentBuild();
+        setTimeout(function() {
+            //navigator.splashscreen.hide();
+            splashElement.setAttribute('style', 'display:none;');
+            beginApp.setAttribute('style', 'display:block;');
+        }, timeDiff);
 
         console.log('Received Event: ' + id);
         console.log('Check: ' + check);
     },
     deviceOnline: function() {
         check = 1;
-        console.log(test);
     },
     deviceOffline: function() {
         check = 2;
+    },
+    contentBuild: function() {
+        for (var i=0; i < physician_obj.physician.length; i++) {
+            document.getElementById("demo").innerHTML += physician_obj.physician[i].firstName + " " + physician_obj.physician[i].lastName + " ";
+            var clinic_name = physician_obj.physician[i].clinic, 
+            locationcheck = 0;
+            for (var j=0; j < clinic_obj.clinic.length; j++) {
+                if (clinic_obj.clinic[j].clinicName == clinic_name) {
+                    document.getElementById("demo").innerHTML += "<b>Address</b>: " + clinic_obj.clinic[j].streetAddress + " </br>";
+                }
+            }
+        }
     }
 };
-
-var physician_data_text = '{ "physician" : [' +
-'{ "firstName":"John" , "lastName":"Doe" , "speciality":"general" , "clinic":"sugarland" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"dentist" , "clinic":"sugarland 2" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"gyna" , "clinic":"sugarland 2" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"cardio" , "clinic":"sugarland 2" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"allergist" , "clinic":"sugarland 2" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"dermatologist" , "clinic":"sugarland 2" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"microbiologist" , "clinic":"sugarland 2" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"neorologist" , "clinic":"sugarland 2" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" , "speciality":"oncologist" , "clinic":"sugarland 2" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" , "speciality":"pediatrician" , "clinic":"richmond" } ]}';
-
-var speciality_data_text = '{ "speciality" : [' +
-'{ "name":"general" },' +
-'{ "name":"dentist" },' +
-'{ "name":"gyna" },' +
-'{ "name":"cardio" },' +
-'{ "name":"allergist" },' +
-'{ "name":"dermatologist" },' +
-'{ "name":"microbiologist" },' +
-'{ "name":"neorologist" },' +
-'{ "name":"oncologist" },' +
-'{ "name":"pediatrician" } ]}';
-
-var clinic_data_text = '{ "clinic" : [' +
-'{ "clinicName":"sugarland" , "streetAddress":"4710 Abingdon Ct" , "city":"sugar land" , "state":"texas" , "zip":"77449" },' +
-'{ "clinicName":"sugarland 2" , "streetAddress":"5322 Mornington Dr" , "city":"sugar land" , "state":"texas" , "zip":"77498" },' +
-'{ "clinicName":"richmond" , "streetAddress":"5114 Stone Island Ct" , "city":"richmond" , "state":"texas" , "zip":"77407" } ]}';
-
-var physician_obj = JSON.parse(physician_data_text);
-var speciality_obj = JSON.parse(speciality_data_text);
-var clinic_obj = JSON.parse(clinic_data_text);
